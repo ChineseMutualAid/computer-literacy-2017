31f5346afb405567db9fce456c3f9b92d3fb9007
Feihong Hsu <feihong.hsu@gmail.com>
2017-02-08 20:47:20 -0600

    Add helpful docstrings

--- tasks.py
+++ tasks.py
@@ -7,12 +7,21 @@ from render import render_template
 
 @task
 def serve(ctx):
+    """
+    Serve the site at localhost:8000 so that you can see the results of your
+    changes without building.
+
+    """
     from app import app
     app.run(port=8000, debug=True)
 
 
 @task
 def serve_build(ctx):
+    """
+    Serve the contents of the build/ directory.
+
+    """
     run('cd {} && python -m http.server'.format(build_dir))
 
 
@@ -24,6 +33,11 @@ https://raw.githubusercontent.com/hakimel/reveal.js/master/js/reveal.js
 
 @task
 def download_dependencies(ctx):
+    """
+    Download the source files needed for reveal.js. You only need to do this if
+    you want to upgrade to a newer version of reveal.js
+
+    """
     import requests
 
     for url in DOWNLOAD_FILES:
@@ -41,12 +55,21 @@ def download_dependencies(ctx):
 
 @task
 def clean(ctx):
+    """
+    Delete all files inside the build directory.
+
+    """
     if build_dir.exists():
         run('rm -rf build/*')
 
 
 @task
 def build(ctx):
+    """
+    Build the static files for the web site and put them inside the build
+    directory.
+
+    """
     clean(ctx)
     for src in site_dir.rglob('*?.*'):
         name = src.name
@@ -58,12 +81,16 @@ def build(ctx):
 
 @task
 def publish(ctx):
+    """
+    Publish the web site to GitHub Pages.
+
+    """
+    build(ctx)
     run('ghp-import -n -p {}'.format(build_dir))
 
 
 def run(cmd):
     subprocess.call(cmd, shell=isinstance(cmd, str))
-
 
 
 def copy_or_generate(src, dest):
